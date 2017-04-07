9c104fd70512367eef8fc2cb48cfbe94f7d82861
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-07 17:31:06 -0500

    div-box display inline-block

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
-var coders = ["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado","Yessenia","Miriam Perralta","Elizabeth Condori",
+var coders = ["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado","Yessenia Huaman","Miriam Mendoza","Elizabeth Condori",
 "Arantza Burga","Grecia Rayme","Janine Vega","Rosario Felix","Daguiana Revoredo","Jenny Velasquez","Nadia Cuadros",
 "Michell More","Marilu Llamocca","Mariel Garcia","Fiorella Cisneros","Geraldine Chauca","Yellitza Rivera","Stephanie Hiyagon",
 "Emma Tapia","Danna Franco","Flor Retamozo","Nathaly Pacheco","Ericka Vidal","Katherine Ortega","Brilly Majuan","Flor Tello",
-"Leslie Avendaño","Cindy Mendoza","Annia Flores","Betsi Loayza","Aida Sulca","Milagros Gutierrez","Nakarid Jave","Angie Cóndor",
-"Maricarmen Rojas","Ariana Cabana","Flor Condori","Mitch Rodríguez","Naomi Villanueva","Mari Castillo","Miriam Peralta",
-"Karin Alejo","Liliana Peña","Ruth Salvador","Maribel","Wendy Reyes","Cinthia","Maria Grecia Cutipa","Ana Durand",
+"Leslie Avendaño","Cindy Mendoza","Annia Flores","Betsi Loayza","Aida Sulca","Milagros Gutierrez","Nakarid Jave","Angie Condor",
+"Maia Rojas","Ariadna Cabana","Flor Condori","Mitch Rodriguez","Naomi Villanueva","Mary Castillo","Miriam Peralta",
+"Karin Alejo","Liliana Peña","Ruth Salvador","Maribel","Wendy Reyes","Cinthia","Grecia Cutipa","Ana Durand",
 "Glisse Jorge","Neiza Nuñez","Sandra Solorzano"];
 
 function addFigure(array){
   for (var i = 0; i < array.length; i++) {
     var principal = document.getElementById("section-principal");
+    var box = document.createElement("div");
+    box.setAttribute("class","div-box");
+    principal.appendChild(box);
     var figure = document.createElement("figure");
     figure.setAttribute("class","figure-coder figure-hov");
-    principal.appendChild(figure);
+    box.appendChild(figure);
     var image = document.createElement("img");
     image.setAttribute("src","assets/img/students/"+ (i+1) + ".png");
     image.setAttribute("title",array[i]);
