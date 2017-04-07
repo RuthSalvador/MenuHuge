b84aaa5af4d55722192cf2541cc7c9c1db0bc59d
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-07 13:03:40 -0500

    all header

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var coders = []
+
+
+var principal = document.getElementById('section-principal');
+var figure = document.createElement('figure');
+principal.appendChild(figure);
+
+var image = document.createElement('img');
+figure.appendChild(image);
+
+var title = document.createElement('figcaption');
+figure.appendChild(title);
